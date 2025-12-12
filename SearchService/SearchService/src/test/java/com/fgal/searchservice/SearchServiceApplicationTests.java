4ad90e25a0aafcce68815b410575add1133a0f15
FiodarHaletskiy <fedorallic@gmail.com>
2025-12-13 02:39:55 +0300

    SearchService

---
+++ SearchService/SearchService/src/test/java/com/fgal/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgal.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
